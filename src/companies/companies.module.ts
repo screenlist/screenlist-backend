66b7c66b739cd62d9c059f28e2c329944b61b09d
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-09-12 20:45:39 +0200

    exports the company entity through module

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Company } from './companies.entity';
+import { CompaniesService } from './companies.service';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Company])]
+	imports: [TypeOrmModule.forFeature([Company])],
+	providers: [CompaniesService],
+	exports: [TypeOrmModule]
 })
 export class CompaniesModule {}
