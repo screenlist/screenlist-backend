a519c43145605ebc49f10c6316e9037ca19ca164
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-09-15 21:36:19 +0200

    adds upload functionality

--- src/stills/stills.module.ts
+++ src/stills/stills.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Still } from './stills.entity';
 import { StillsService } from './stills.service';
+import { StillsController } from './stills.controller';
+import { PostersModule } from '../posters/posters.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Still])],
+	imports: [TypeOrmModule.forFeature([Still]), PostersModule],
 	providers: [StillsService],
-	exports: [TypeOrmModule]
+	exports: [TypeOrmModule],
+	controllers: [StillsController]
 })
 export class StillsModule {}
--- src/stills/stills.service.ts
+++ src/stills/stills.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { Storage } from '@google-cloud/storage';
+import * as sharp from 'sharp';
+import * as path from 'path';
+import { Poster } from '../posters/posters.entity';
 
 @Injectable()
-export class StillsService {}
+export class StillsService {
+	constructor(private configService: ConfigService){}
+
+	private storage = new Storage({
+		projectId: this.configService.get('PROJECT_ID'),
+		keyFilename: path.join(__dirname, '../../cloud-keys.json')
+	})
+
+	async uploadPoster(poster: Express.Multer.File):Promise<any>{
+		if(poster.mimetype === 'image/png' || poster.mimetype === 'image/jpeg'){
+			const posterBucket = this.storage.bucket(this.configService.get('STORAGE_POSTERS'));
+			const blob = posterBucket.file(poster.originalname.replace(/[^0-9a-z]/gi, ''));
+			try{
+				const buffer = await sharp(poster.buffer).resize(1000, 1500).toBuffer()
+				await blob.save(buffer, {contentType: poster.mimetype})
+				return {originalName: blob.name, url: `https://storage.googleapis.com/${posterBucket.name}/${blob.name}`}
+			} catch(err: any) {
+				throw new BadRequestException(err?.message)
+			}
+		}
+		throw new BadRequestException("Unrecognised file extension")
+	}
+
+	async uploadStill(still: Express.Multer.File): Promise<any>{
+		const stillBucket = this.storage.bucket(this.configService.get('STORAGE_STILLS'));
+		const blob = stillBucket.file(still.originalname);
+		const blobStream = blob.createWriteStream({
+		  resumable: false,
+		});
+		blobStream.on('finish', () => {console.log(`https://storage.googleapis.com/${stillBucket.name}/${blob.name}`)})
+	}
+}
