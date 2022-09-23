f4613b9cce014fbfb522d653e1e70808b54b52fc
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-09-23 18:50:05 +0200

    lays ground for the work that's about to begin

--- src/series/series.service.ts
+++ src/series/series.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { 
+	Injectable, 
+	ParseFileOptions, 
+	BadRequestException, 
+	NotFoundException, 
+	UnauthorizedException 
+} from '@nestjs/common';
+import { DatabaseService } from '../database/database.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class SeriesService {}
+export class SeriesService {
+	constructor(
+		private configService: ConfigService,
+		private datastore: DatabaseService
+	){
+		this.datastore = new DatabaseService(configService)
+	}
+}
