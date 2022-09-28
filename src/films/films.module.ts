798b25a906722af3ee915192aff1329918235bd1
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-09-28 20:29:21 +0200

    removes deleted modules

--- src/films/films.module.ts
+++ src/films/films.module.ts
@@ -3,20 +3,16 @@ import { FilmsController } from './films.controller';
 import { FilmsService } from './films.service';
 import { CompaniesModule } from '../companies/companies.module';
 import { UsersModule } from '../users/users.module';
-import { StillsModule } from '../stills/stills.module';
 import { PlatformsModule } from '../platforms/platforms.module';
 import { PeopleModule } from '../people/people.module';
-import { PostersModule } from '../posters/posters.module';
-import { DatabaseModule } from '../database/database.module'
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
 	imports: [
 		CompaniesModule,
 		UsersModule,
-		StillsModule,
 		PlatformsModule,
 		PeopleModule,
-		PostersModule,
 		DatabaseModule
 	],
 	controllers: [FilmsController],
