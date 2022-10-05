05a4b9c71f3e5d21bb80c4e327b34dfb23304a4b
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-10-05 18:45:33 +0200

    refactors methods and adds delete

--- src/series/series.service.ts
+++ src/series/series.service.ts
@@ -6,41 +6,43 @@ import {
 	UnauthorizedException 
 } from '@nestjs/common';
 import { DatabaseService } from '../database/database.service';
+import { StorageService } from '../storage/storage.service';
 import { ConfigService } from '@nestjs/config';
 import { CreateSeriesDto, UpdateSeriesDto } from './series.dto';
 import { 
 	Poster, 
-	Still, 
-	PersonRole, 
-	Person,
-	Link,
-	Platform
+	Still
 } from '../films/films.types';
-import { SeriesType, SeriesDetails } from './series.types';
+import { SeriesType, Series } from './series.types';
+import { Person, PersonRole } from '../people/people.types';
+import { Link, Platform } from '../platforms/platforms.types';
 import { Company, CompanyRole } from '../companies/companies.types';
+import { HistoryOpt } from '../database/database.types';
 
 @Injectable()
 export class SeriesService {
 	constructor(
 		private configService: ConfigService,
-		private datastore: DatabaseService
+		private storage: StorageService,
+		private db: DatabaseService
 	){
-		this.datastore = new DatabaseService(configService)
+		this.db = new DatabaseService(configService);
+		this.storage = new StorageService(configService);
 	}
 
 	async findAll(): Promise<SeriesType[]>{
-		const query = this.datastore.createQuery('Series').filter('status', '=', 'public').limit(20)
+		const query = this.db.createQuery('Series').filter('status', '=', 'public').limit(20)
 		const results = []
 		try {
-			const series = await this.datastore.runQueryFull(query)
+			const series = await this.db.runQueryFull(query)
 			// Loop through each series to retrieve its poster
-			series[0].forEach(async (series: SeriesDetails) => {
-				const seriesKey = this.datastore.key(['Series', series.id])
-				const posterQuery = this.datastore.createQuery('Poster')
+			series[0].forEach(async (series: Series) => {
+				const seriesKey = this.db.key(['Series', series.id])
+				const posterQuery = this.db.createQuery('Poster')
 					.hasAncestor(seriesKey)
 					.filter('quality', '=', 'SD')
 					.limit(1);
-				const posters: Poster[] = await this.datastore.runQueryFull(posterQuery);
+				const posters: Poster[] = await this.db.runQueryFull(posterQuery);
 				const wholeFilm: SeriesType = {
 					details: series,
 					posters: posters
@@ -54,56 +56,56 @@ export class SeriesService {
 	}
 
 	async findOne(id: number, user: string): Promise<SeriesType>{
-		const seriesKey = this.datastore.key(['Series', id]);
+		const seriesKey = this.db.key(['Series', id]);
 		// Create queries
-		const postersQuery = this.datastore.createQuery('Poster')
+		const postersQuery = this.db.createQuery('Poster')
 			.hasAncestor(seriesKey)
 			.filter('quality', '=', 'HD')
 			.order('created', {descending: true})
 			.limit(1);
-		const linksQuery =this.datastore.createQuery('Link')
+		const linksQuery =this.db.createQuery('Link')
 			.hasAncestor(seriesKey)
 			.order('created', {descending: true});
-		const stillsQuery = this.datastore.createQuery('Still')
+		const stillsQuery = this.db.createQuery('Still')
 			.hasAncestor(seriesKey)
 			.filter('quality','=', 'HD')
 			.order('created', {descending: true})
 			.limit(6);
-		const producersQuery = this.datastore.createQuery('CompanyRole')
+		const producersQuery = this.db.createQuery('CompanyRole')
 			.hasAncestor(seriesKey)
 			.filter('type', '=', 'production')
 			.order('companyName');
-		const actorsQuery = this.datastore.createQuery('PersonRole')
+		const actorsQuery = this.db.createQuery('PersonRole')
 			.hasAncestor(seriesKey)
 			.filter('category', '=', 'talent')
 			.order('personName');
-		const crewQuery = this.datastore.createQuery('PersonRole')
+		const crewQuery = this.db.createQuery('PersonRole')
 			.hasAncestor(seriesKey)
 			.filter('category', '=', 'crew')
 			.order('title');
 
 		try {
 			// Run queries
-			const details = await this.datastore.get(seriesKey);
+			const details = await this.db.get(seriesKey);
 			// Check whether the series is public or hidden before continuing
 			if(details[0].status != "public"){
 				throw new NotFoundException("Not available");
 			}
-			const platformLinks: Link[] =  await this.datastore.runQueryFull(linksQuery);
-			const poster = await this.datastore.runQuery(postersQuery);
-			const stills = await this.datastore.runQuery(stillsQuery);
-			const producers: CompanyRole[] = await this.datastore.runQueryFull(producersQuery);
-			const actors: PersonRole[] = await this.datastore.runQueryFull(actorsQuery);
-			const crew: PersonRole[] = await this.datastore.runQueryFull(crewQuery);
+			const platformLinks: Link[] =  await this.db.runQueryFull(linksQuery);
+			const poster = await this.db.runQuery(postersQuery);
+			const stills = await this.db.runQuery(stillsQuery);
+			const producers: CompanyRole[] = await this.db.runQueryFull(producersQuery);
+			const actors: PersonRole[] = await this.db.runQueryFull(actorsQuery);
+			const crew: PersonRole[] = await this.db.runQueryFull(crewQuery);
 
 			// Extact the entity id/name from query to expose to the client
 			details.map(obj => {
-				obj.id = obj[this.datastore.KEY]["id"]
+				obj.id = obj[this.db.KEY]["id"]
 				return obj
 			})
 
 			const film: SeriesType = {
-				details: details[0] as SeriesDetails,
+				details: details[0] as Series,
 				posters: poster[0] as Poster[],
 				stills: stills[0] as Still[],
 				producers: producers,
@@ -119,149 +121,174 @@ export class SeriesService {
 	}
 
 	async createOne(series: CreateSeriesDto, user: string){
-		const transactionId = (new Date()).toISOString().concat("-create-series-"+series.details.name);
 		// A variable to house all entities created
 		let entities = []
 		// Creates the series details entity
-		const seriesKey = this.datastore.key('Series');
-		const filmName = series.details.name;
+		const seriesKey = this.db.key('Series');
+		const filmName = series.name;
 		const time = new Date();
-		series.details.slug = filmName.concat("-"+seriesKey.id.toString());
-		series.details.lastUpdated = time;
-		series.details.created = time;
-		series.details.status = "public"
-		series.details.nameEditable = true;
+		series.slug = filmName.concat("-"+seriesKey.id.toString());
+		series.lastUpdated = time;
+		series.created = time;
+		series.status = "public";
 		entities.push({
 			key: seriesKey,
-			data: series.details
+			data: series
 		})
 		// Write series action into history
-		entities.push(this.datastore.formulateHistory(series.details, 'Series',	seriesKey.id,	user,	'create'));
-
-		// Creates poster entities
-		series.posters.forEach(poster => {
-			const posterEntity = this.datastore.createPosterEntity(poster, seriesKey.id, time, 'Series');
-			entities.push(posterEntity)
-			// Write poster action into history
-			entities.push(this.datastore.formulateHistory(poster,	'Poster',	posterEntity.key.name,	user,	'create'));
-		})
-		// Creates still frame entities
-		series.stillFrames.forEach(frame => {
-			const still = this.datastore.createStillEntity(frame, seriesKey.id, time, 'Series');
-			entities.push(still)
-			// Write film action into history
-			entities.push(this.datastore.formulateHistory(frame, 'Still', still.key.name, user, 'create'));
-		})
-		// Creates person role entities
-		series.credits.forEach(async (role) => {
-			const data = await this.datastore.createPersonRoleEntity(role, seriesKey.id, time, user, 'Series');
-			entities.push(...data.entities);
-			entities.push(...data.history);
-		})
-		// Creates company role entities
-		series.companies.forEach(async (role) => {
-			const data = await this.datastore.createCompanyRoleEntity(role, seriesKey.id, time, user, 'Series');
-			entities.push(...data.entities);
-			entities.push(...data.history);
-		})
-		// Creates link entites
-		series.currentPlatforms.forEach(async (link) => {
-			const data = await this.datastore.createLinkEntity(link, seriesKey.id, time, user, 'Series');
-			entities.push(...data.entities);
-			entities.push(...data.history);
-		})
+		const historyObj: HistoryOpt = {
+			data: series,
+			user: user,
+			time: time,
+			action: 'create',
+			kind: 'Series',
+			id: seriesKey.id
+		}
+		entities.push(this.db.formulateHistory(historyObj));
 
 		try {
-			await this.datastore.transaction({ id: transactionId }).insert(entities);
+			await this.db.transaction().insert(entities);
 			return {"status": "successfully created"}
 		} catch(err: any){
 			throw new BadRequestException(err.message);
 		}
 	}
 
-	async updateOne(series: UpdateSeriesDto, user: string){
-		const transactionId = (new Date()).toISOString().concat('-updated-series-'+series.details.name);
-		const transactionHistoryId = transactionId+"-history";
-		const time = new Date()
-		let history = [] // Where to put all history entities
-		let entities = [] // Where to put all entities needing an update
-		const seriesKey = this.datastore.key(['Series', series.details.id]);
-		const state = await this.datastore.get(seriesKey) // to check if the film really exists
-		if(state.length !>= 1 && isNaN(state[0]) == false){
-			throw new NotFoundException('This film does not exist')
-		}
-		if(series.details.name && state[0].nameEditable == false){
-			throw new UnauthorizedException('You are not authorised to edit the film name');
-		}
-		if(series.details.slug){
-			throw new UnauthorizedException('You are not authorised to edit the slug');
-		}
-		delete series.details.id; // deletes the id from entity
-		if(Object.keys(series.details).length >= 1){
-			series.details.lastUpdated = time
-			entities.push({
-				key: seriesKey,
-				data: series.details
-			})
+	async updateOne(series: UpdateSeriesDto, user: string, id: string){
+		const time = new Date();
+		const seriesKey = this.db.key(['Series', +id]);
 
-			// Create history
-			history.push(this.datastore.formulateHistory(series.details, 'Series', seriesKey.id, user, 'update'));
+		if(series.name || series.slug){
+			series.slug = series.name.concat("-"+seriesKey.id);
+		}		
+		
+		series.lastUpdated = time
+		const entity = {
+			key: seriesKey,
+			data: series
 		}
 
-		// Updates posters
-		if(series.posters && series.posters.length <= 2){
-			series.posters.forEach((poster) => {
-				const data = this.datastore.updatePosterEntity(poster, seriesKey.id, time, 'Series');
-				entities.push(data);
-
-				// Create history
-				history.push(this.datastore.formulateHistory(poster, 'Poster', data.key.name, user, 'update'));
-			})
+		// Create history
+		const historyObj: HistoryOpt = {
+			data: series,
+			user: user,
+			time: time,
+			action: 'update',
+			kind: 'Series',
+			id: seriesKey.id
 		}
-
-		// Updates stills
-		if(series.stillFrames && series.stillFrames.length <= 3){
-			series.stillFrames.forEach((still) => {
-				const data = this.datastore.updateStillEntity(still, seriesKey.id, time, 'Series');
-				entities.push(data);
-
-				// Create history
-				history.push(this.datastore.formulateHistory(still, 'Still', data.key.name, user, 'update'));
-			})
-		}
-
-		// Updates company roles
-		if(series.companies){
-			series.companies.forEach(async (role) => {
-				// For company role of an existing company
-				const data = await this.datastore.updateCompanyRoleEntity(role, seriesKey.id, time, user, 'Series');
-				entities.push(...data.entities);
-				history.push(...data.history);
-			})
-		}
-
-		// Updates person roles
-		if(series.credits){
-			series.credits.forEach(async (role) => {
-				const data = await this.datastore.updatePersonRoleEntity(role, seriesKey.id, time, user, 'Series');
-				entities.push(...data.entities);
-				history.push(...data.history);
-			})
-		}
-
-		// Updates watch links
-		if(series.currentPlatforms){
-			series.currentPlatforms.forEach(async (link) => {
-				const data = await this.datastore.updateLinkEntity(link, seriesKey.id, time, user, 'Series');
-				entities.push(...data.entities);
-				history.push(...data.history);
-			})
-		}
-
+		const history = this.db.formulateHistory(historyObj);
+		
 		try{
-			await this.datastore.transaction({id: transactionId}).update(entities);
-			await this.datastore.transaction({id: transactionHistoryId}).insert(history);
+			await this.db.transaction().update(entity);
+			await this.db.transaction().insert(history);
 			return { 'status': 'successfully updated' };
+		} catch(err: any){
+			throw new BadRequestException(err.message)
+		}
+	}
+
+	async deleteOne(id: string, user: string){
+		const deletion = []
+		const history = []
+		const time = new Date();
+		const seriesKey = this.db.key(['Series', +id]);
+		const postersQuery = this.db.createQuery('Poster').hasAncestor(seriesKey);
+		const linksQuery =this.db.createQuery('Link').hasAncestor(seriesKey);
+		const stillsQuery = this.db.createQuery('Still').hasAncestor(seriesKey);
+		const companiesRolesQuery = this.db.createQuery('CompanyRole').hasAncestor(seriesKey);
+		const peopleRolesQuery = this.db.createQuery('PersonRole').hasAncestor(seriesKey);
+		try {
+			const [posters] = await this.db.runQuery(postersQuery);
+			const [stills] = await this.db.runQuery(stillsQuery);
+			// Deletes the actual files before adding entities
+			// to the deletion array
+			posters.forEach(async (poster: Poster) => {
+				const removal = await this.storage.deletePoster(poster.originalName);
+				if(removal){
+					deletion.push(poster);
+					const historyObj: HistoryOpt = {
+						data: poster,
+						user: user,
+						kind: 'Poster',
+						id: poster[this.db.KEY]['id'],
+						action: 'delete',
+						time: time,
+					}
+					history.push(this.db.formulateHistory(historyObj));
+				}
+			})
+			stills.forEach(async (still: Still) => {
+				const removal = await this.storage.deletePoster(still.originalName);
+				if(removal){
+					deletion.push(still);
+					const historyObj: HistoryOpt = {
+						data: still,
+						user: user,
+						kind: 'Still',
+						id: still[this.db.KEY]['id'],
+						action: 'delete',
+						time: time,
+					}
+					history.push(this.db.formulateHistory(historyObj));
+				}
+			})
+
+			const [links] = await this.db.runQuery(linksQuery);
+			const [companiesRoles] = await this.db.runQuery(companiesRolesQuery);
+			const [peopleRoles] = await this.db.runQuery(peopleRolesQuery);
+
+			links.forEach((link: Link) => {
+				deletion.push(link);
+				const historyObj: HistoryOpt = {
+					data: link,
+					user: user,
+					kind: 'Link',
+					id: link[this.db.KEY]['id'],
+					action: 'delete',
+					time: time,
+				}
+				history.push(this.db.formulateHistory(historyObj));
+			})
+			companiesRoles.forEach((role: CompanyRole) => {
+				deletion.push(role);
+				const historyObj: HistoryOpt = {
+					data: role,
+					user: user,
+					kind: 'CompanyRole',
+					id: role[this.db.KEY]['id'],
+					action: 'delete',
+					time: time,
+				}
+				history.push(this.db.formulateHistory(historyObj));
+			})
+			peopleRoles.forEach((role: PersonRole) => {
+				deletion.push(role);
+				const historyObj: HistoryOpt = {
+					data: role,
+					user: user,
+					kind: 'PersonRole',
+					id: role[this.db.KEY]['id'],
+					action: 'delete',
+					time: time
+				}
+				history.push(this.db.formulateHistory(historyObj));
+			})
+			const [series] = await this.db.get(seriesKey);
+			deletion.push(series);
+			// Write action into history
+			const historyObj: HistoryOpt = {
+				data: series,
+				user: user,
+				kind: 'Series',
+				id: seriesKey.id,
+				action: 'delete',
+				time: time,
+			}
+			history.push(this.db.formulateHistory(historyObj));
+			await this.db.transaction().delete(deletion);
+			await this.db.transaction().insert(history);
+			return {'status': 'deleted'}
 		} catch(err: any){
 			throw new BadRequestException(err.message)
 		}
