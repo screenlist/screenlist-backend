02f5e879c2bbbbc9e08ce08ebaff061e1b8cdd4f
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-10-09 21:03:50 +0200

    exports the service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [AuthService],
   exports: [AuthService]
 })
