3cc2320ed2c054a856f987d4ab6c5df8c9e1188b
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-10-09 21:05:11 +0200

    adds auth module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { User } from './users.entity';
+import { AuthModule } from '../auth/auth.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [AuthModule, DatabaseModule],
   providers: [UsersService],
   controllers: [UsersController]
 })
