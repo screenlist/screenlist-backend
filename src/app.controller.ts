3262e6166886afadbf61116f85c37354c0765661
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2023-07-11 08:19:36 +0200

    fix image extraction

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, UseGuards, Body } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Body, Query, StreamableFile, Res, BadRequestException } from '@nestjs/common';
 import { AppService } from './app.service';
 import { RolesGuard } from './users/roles.guard';
 import { Roles } from './users/roles.decorator';
+import type { Response } from 'express';
 
 @Controller()
 @UseGuards(RolesGuard)
@@ -16,6 +17,21 @@ export class AppController {
 	@Post('refresh')
 	@Roles('member')
 	async refreshClient(@Body('path') path: string){
-		return await this.appService.refreshClient(path)
+		return await this.appService.refreshClient(path);
+	}
+
+	@Post('extract')
+	@Roles('member')
+	async getBlob(
+		@Query('path') path: string,
+		@Res({ passthrough: true}) res: Response
+	){
+		try{
+			const values = await this.appService.getImage(path);
+			res.set({'Content-Type': values.type});
+			return new StreamableFile(values.uint8)
+		} catch (err: any){
+			throw new BadRequestException(err.message)
+		}
 	}
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,23 +1,40 @@
 import { ConfigService } from '@nestjs/config';
-import { Injectable, BadRequestException } from '@nestjs/common';
-import axios from 'axios'
+import { Injectable, BadRequestException, StreamableFile } from '@nestjs/common';
+import axios from 'axios';
+import fetch from 'cross-fetch';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
+	constructor(private configService: ConfigService) {}
 
-  getHello(): string {
-    return 'Hello World!';
-  }
+	getHello(): string {
+		return 'Hello World!';
+	}
 
-  async refreshClient(path: string) {
-    const url = `${this.configService.get('CLIENT_URL')}/api/revalidate?secret=${this.configService.get('CLIENT_REVALIDATION_TOKEN')}&path=${path}`
+	async refreshClient(path: string) {
+		const url = `${this.configService.get('CLIENT_URL')}/api/revalidate?secret=${this.configService.get('CLIENT_REVALIDATION_TOKEN')}&path=${path}`
+		try {
+			if(!path) { throw new BadRequestException('Provide the path') }
+			await axios.get(url)
+			return { status: 'refreshed' }
+		} catch (err: any) {
+			throw new BadRequestException(err.message)
+		}
+	}
+
+	async getImage(path: string) {
     try {
       if(!path) { throw new BadRequestException('Provide the path') }
-      await axios.get(url)
-      return { status: 'refreshed' }
+      const res = await fetch(path);
+    	if(res.status >= 400){ throw new BadRequestException('Error Extracting Image') }
+    	const blob = await res.blob()
+    	if(blob.type == 'image/png' || blob.type == 'image/jpeg'){
+	    	const unit8 = new Uint8Array(await blob.arrayBuffer())
+	    	return {uint8: unit8, type: blob.type}
+	    } else { throw new BadRequestException('Unsupported File Extension') }
     } catch (err: any) {
-      console.log(err)
       throw new BadRequestException(err.message)
     }
   }
