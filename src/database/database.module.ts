1e40599937d6f122c413d9b6f3cb0693ee81f607
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2023-07-11 08:30:07 +0200

    fixes bugs, refactors multiple methods and adds new ones

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { Datastore } from '@google-cloud/datastore'
 import { DatabaseService } from './database.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [Datastore],
+  imports: [Datastore, AuthModule],
   providers: [DatabaseService],
   exports: [DatabaseService]
 })
--- src/database/database.types.ts
+++ src/database/database.types.ts
@@ -1,7 +1,10 @@
 export interface HistoryOpt {
+	prevDataObject?: any,
 	dataObject: any,
 	kind: string,
+	pKind?: string,
 	id: string,
+	pId?: string,
 	user: string,
 	time: Date,
 	action: string
