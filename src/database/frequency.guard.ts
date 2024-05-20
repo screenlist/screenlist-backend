70bf3b5d4ca21456e3a570a7896267d9442e9f40
alexkokobane <63163413+alexkokobane@users.noreply.github.com>
2024-05-20 20:44:52 +0200

    setup the optional auth

--- src/database/frequency.guard.ts
+++ src/database/frequency.guard.ts
@@ -1,13 +1,18 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core'
 import { DatabaseService } from './database.service'
+import { AuthService } from 'src/auth/auth.service';
 import { Collection } from './database.types';
+import { ConfigService } from '@nestjs/config';
+import { UserExt } from 'src/users/users.types';
 
 @Injectable()
 export class FrequencyGuard implements CanActivate {
 	constructor(
 		private reflector: Reflector,
-		private mongo: DatabaseService
+		private mongo: DatabaseService,
+		private authService: AuthService,
+		private config: ConfigService
 	){}
 
 	async canActivate(
@@ -17,13 +22,26 @@ export class FrequencyGuard implements CanActivate {
 		const request =  ctx.switchToHttp().getRequest();
 		const path: string = request.url
 
-		if(!kind){ return true } else {
-			const id = path.split('/')[2];
-			const real = await this.mongo.db.collection(kind).findOne({id: id})
-			if(real){
-				await this.mongo.addHit(kind, id);			
+		try {
+			if(request.headers['authorization']){
+				const jwt = await this.authService.client.verifyToken(request.headers['authorization'].split(' ')[1])
+				const unixTimestamp = Math.floor(Date.now()/1000)
+				const clientHost = this.config.get('CLIENT_URL')
+				if( unixTimestamp < jwt.exp && jwt.nbf < unixTimestamp && jwt.azp === clientHost){ 
+					request.headers['x-user-id'] = jwt.sub
+				}
+			}			
+
+			if(!kind){ return true } else {
+				const id = path.split('/')[2];
+				const real = await this.mongo.db.collection(kind).findOne({id: id})
+				if(real){
+					await this.mongo.addHit(kind, id);			
+				}
+				return true;
 			}
-			return true;
-		}
+		} catch(err: any){ console.log(err); true; }
+
+		
 	}
 }
