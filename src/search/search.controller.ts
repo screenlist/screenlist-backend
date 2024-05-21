24d406995e2d8bbcfd37e2a0950cac78cc094c3f
alexkokobane <63163413+alexkokobane@users.noreply.github.com>
2024-05-21 14:06:24 +0200

    removed circular dependency

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -16,30 +16,11 @@ import { Roles } from '../users/roles.decorator';
 import { SearchService } from './search.service'
 
 @Controller('search')
-@UseGuards(RolesGuard)
 export class SearchController {
 	constructor(private search: SearchService){}
 	
 	@Get()
 	async searchFunction(){
 		return await this.search.getAllCollections()
-	}
-
-	@Post('create')
-	@Roles('admin')
-	async create(){
-		return await this.search.createCollections()
-	}
-
-	@Post('index')
-	@Roles('admin')
-	async index(){
-		return await this.search.indexAll()
-	}
-
-	@Post('delete')
-	@Roles('admin')
-	async delete(){
-		return await this.search.deleteAllCollections()
 	}
 }
